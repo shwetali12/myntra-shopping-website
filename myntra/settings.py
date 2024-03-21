d3c85c0f89b4e5d0b1f10e77ddf024a693540782
shwetali <shwetalipoman@gmail.com>
2024-03-21 16:18:56 +0530

    second

--- myntra/settings.py
+++ myntra/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-o*p$-bt$7=l28o^sl%a@1ea4mgv9_&^vb0!mb9vu@(rniejws9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 AUTH_USER_MODEL = 'app1.CustomUser'
 
 # Application definition
